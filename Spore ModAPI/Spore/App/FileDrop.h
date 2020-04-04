b84328815d60ea9354460aa907caf55b8fbb1774
Emd4600 <emd4600@gmail.com>
2020-04-04 20:42:25 +0200

    Added support for drag-and-drop files into Spore

---
+++ Spore ModAPI/Spore/App/FileDrop.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <EASTL\vector.h>
+#include <EASTL\string.h>
+#include <EASTL\intrusive_ptr.h>
+
+#define FileDropMessagePtr intrusive_ptr<App::FileDropMessage>
+
+namespace App
+{
+	using namespace eastl;
+
+	class FileDropMessage
+	{
+	public:
+		static const uint32_t ID = 0x24CE123;
+
+		virtual ~FileDropMessage() = 0;
+		virtual int AddRef() = 0;
+		virtual int Release() = 0;
+
+		/* 00h */	int field_0;
+		/* 04h */	vector<string16> files;
+		/* 18h */	int field_18;
+		/* 1Ch */	int field_1C;
+	};
+}
