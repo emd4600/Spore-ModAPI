05052e08d32c954178304d0323db2e9f0d6cf28c
Emd4600 <emd4600@gmail.com>
2020-04-14 13:35:34 +0200

    Completed cAssetMetadata class

--- Spore ModAPI/Spore/Pollinator/cAssetMetadata.h
+++ Spore ModAPI/Spore/Pollinator/cAssetMetadata.h
@@ -10,33 +10,52 @@
 
 namespace Pollinator
 {
+	// documented from https://spore-community.github.io/docs/pollination/pollen_metadata.html#use-in-package-files
 	class cAssetMetadata
 		: public Resource::ResourceObject
 	{
 	public:
 		const static uint32_t TYPE = 0x30BDEE3;
 
+		int64_t GetAssetID() const;
+		int64_t GetParentAssetID() const;
+		int64_t GetOriginalParentAssetID() const;
+
+		const ResourceKey& GetAssetKey() const;
+		const ResourceKey& GetParentAssetKey() const;
+
+		int64_t GetTimeCreated();
+		int64_t GetTimeDownloaded();
+
 		string16 GetName() const;
 		string16 GetDescription() const;
 		string16 GetAuthor() const;
+		int64_t GetAuthorID() const;
+		const vector<string16>& GetAuthors() const;
+		const vector<string16>& GetTags() const;
+
+		const vector<uint32_t>& GetConsequenceTraits() const;
+
+		bool IsShareable() const;
+		bool IsLocalized() const;
 
 	public:
-		/* 18h */	int field_18;  // -1
-		/* 1Ch */	int field_1C;  // -1
-		/* 20h */	ResourceKey field_20;
-		/* 2Ch */	ResourceKey field_2C;  // -1, -1, -1
-		/* 38h */	int64_t field_38;  // -1
-		/* 40h */	int64_t field_40;  // -1
-		/* 48h */	int64_t field_48;
-		/* 50h */	int64_t field_50;
+		/* 18h */	int64_t mAssetID;  // -1
+		/* 20h */	ResourceKey mAssetKey;
+		/* 2Ch */	ResourceKey mParentAssetKey;  // -1, -1, -1
+		/* 38h */	int64_t mParentAssetID;  // -1
+		/* 40h */	int64_t mOriginalParentAssetID;  // -1
+		/* 48h */	int64_t mTimeCreated;
+		/* 50h */	int64_t mTimeDownloaded;
 		/* 58h */	string16 mAuthorName;
-		/* 68h */	ResourceKey field_68;  // -1, -1, -1
-		/* 74h */	bool field_74;  // true
+		/* 68h */	int64_t mAuthorID;
+		/* 70h */	int mUseLocale;  // 0xFFFFFFFF if not localized, 1 if localized
+		/* 74h */	bool mIsShareable;  // true
 		/* 78h */	string16 mName;
 		/* 88h */	string16 mDescription;
 		/* 98h */	vector<string16> mAuthors;
 		/* ACh */	vector<string16> mTags;
-		/* C0h */	vector<int> field_C0;
+		/* C0h */	vector<uint32_t> mConsequenceTraits;
 	};
 
 	ASSERT_SIZE(cAssetMetadata, 0xD8);
@@ -53,4 +72,22 @@ namespace Pollinator
 	inline string16 cAssetMetadata::GetName() const { return mName; }
 	inline string16 cAssetMetadata::GetDescription() const { return mDescription; }
 	inline string16 cAssetMetadata::GetAuthor() const { return mAuthorName; }
+	inline int64_t cAssetMetadata::GetAuthorID() const { return mAuthorID; }
+	inline const vector<string16>& cAssetMetadata::GetAuthors() const { return mAuthors; }
+	inline const vector<string16>& cAssetMetadata::GetTags() const { return mTags; }
+
+	inline const vector<uint32_t>& cAssetMetadata::GetConsequenceTraits() const { return mConsequenceTraits; }
+
+	inline bool cAssetMetadata::IsShareable() const { return mIsShareable; }
+	inline bool cAssetMetadata::IsLocalized() const { return mUseLocale == 1; }
+
+	inline int64_t cAssetMetadata::GetAssetID() const { return mAssetID; }
+	inline int64_t cAssetMetadata::GetParentAssetID() const { return mParentAssetID; }
+	inline int64_t cAssetMetadata::GetOriginalParentAssetID() const { return mOriginalParentAssetID; }
+
+	inline const ResourceKey& cAssetMetadata::GetAssetKey() const { return mAssetKey; }
+	inline const ResourceKey& cAssetMetadata::GetParentAssetKey() const { return mParentAssetKey; }
+
+	inline int64_t cAssetMetadata::GetTimeCreated() { return mTimeCreated; }
+	inline int64_t cAssetMetadata::GetTimeDownloaded() { return mTimeDownloaded; }
 }
