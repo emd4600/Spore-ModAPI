1b3dbc12d53651da2d154c256c7569cf4c23d136
emd4600 <emd4600@gmail.com>
2020-07-26 11:21:14 +0200

    More animation stuff.

---
+++ Spore ModAPI/Spore/Anim/SPAnimation.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstdint>
+#include <EASTL\intrusive_ptr.h>
+
+#define SPAnimationPtr eastl::intrusive_ptr<Anim::SPAnimation>
+
+namespace Anim
+{
+	struct SPAnimation
+	{
+		int AddRef();
+		int Release();
+
+		/* 00h */	uint32_t magic;  // 'ANIM'
+		/* 04h */	int fileSize;
+		/* 08h */	int version;
+		/* 0Ch */	char path[256];
+		/* 10Ch */	int field_10C;
+		/* 110h */	uint32_t id;
+		/* 114h */	int mnRefCount;
+		/* 118h */	int field_118;
+		/* 11Ch */	float frameLength;  // 1/30
+		/* 120h */	float length;
+		/* 124h */	char padding_124[0x13C - 0x124];
+		/* 13Ch */	int vfxCount;
+		/* 140h */	void* pVFX;
+		/* 144h */	int channelCount;
+		/* 148h */	void* pChannels;
+	};
+
+	inline int SPAnimation::AddRef() {
+		++mnRefCount;
+		return mnRefCount;
+	}
+
+	inline int SPAnimation::Release() {
+		--mnRefCount;
+		if (mnRefCount == 0) {
+			delete this;
+			return 0;
+		}
+		return mnRefCount;
+	}
+}
---
+++ Spore ModAPI/Spore/Anim/cAnimManagerImpl.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Spore\Anim\IAnimManager.h>
+#include <Spore\Anim\SPAnimation.h>
+#include <Spore\App\IMessageListener.h>
+
+namespace Anim
+{
+	class cAnimManagerImpl
+		: public IAnimManager
+		, public App::IMessageListener
+		, public DefaultRefCounted
+	{
+	public:
+		/* 10h */	float field_10;  // 0.002
+		/* 14h */	SPAnimationPtr mpDefaultFillerAnimation;
+		/* 18h */	map<int, int> field_18;
+		/* 34h */	map<uint32_t, SPAnimationPtr> mPendingAnimMap;
+		/* 50h */	int field_50;
+		/* 54h */	int field_54;
+		/* 58h */	int field_58;
+		/* 5Ch */	int field_5C;
+		/* 60h */	int field_60;
+		/* 64h */	int field_64;
+		/* 68h */	vector<IAnimWorldPtr> mWorlds;
+		/* 7Ch */	int field_7C;
+		/* 80h */	int field_80;
+		/* 84h */	int field_84;
+	};
+	ASSERT_SIZE(cAnimManagerImpl, 0x88);
+}
