2221e405700fa18c0bd4daa4707fcb3ead7825d2
emd4600 <emd4600@gmail.com>
2023-06-09 11:42:58 +0200

    Fixed the UTFWin::Message::Mouse IsLeftButton(), IsMiddleButton() and IsRightButton()

--- Spore ModAPI/Spore/UTFWin/Message.h
+++ Spore ModAPI/Spore/UTFWin/Message.h
@@ -117,17 +117,17 @@ namespace UTFWin
 				/* 10h */	float mouseY;
 				/// A combination of values from the MouseStateFlags enum.
 				/* 14h */	int mouseState;
-				/// Which mouse button was pressed/released.
+				/// Which mouse button was pressed/released. Not always available??
 				/* 18h */	MouseButton mouseButton;
 
 				inline bool IsLeftButton() const {
-					return mouseButton == MouseButton::kMouseButtonLeft;
+					return (mouseState & MouseStateFlags::kMouseLeftButtonDown) != 0;
 				}
 				inline bool IsMiddleButton() const {
-					return mouseButton == MouseButton::kMouseButtonWheel;
+					return (mouseState & MouseStateFlags::kMouseMiddleButtonDown) != 0;
 				}
 				inline bool IsRightButton() const {
-					return mouseButton == MouseButton::kMouseButtonRight;
+					return (mouseState & MouseStateFlags::kMouseRightButtonDown) != 0;
 				}
 				inline Math::Point GetPosition() const {
 					return { mouseX, mouseY };
