3c3ed76b9da2d2a8d5ae5b41593a5597cd4a4247
Allison Ghost <allisongrunder@gmail.com>
2024-08-25 16:02:17 -0700

    Add 2 new dev cheats (devEffectLog & PrintCursor), add support for palette subcategories in part icon capture.
    NOTE:  UNTESTED!
    For those who can, please compile this and test it ingame before accepting/rejecting PR.

---
+++ Projects/Example Projects/ModCreatorKit/PrintCursorCheat.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "PrintCursorCheat.h"
+
+void PrintCursorCheat::ParseLine(const ArgScript::Line& line)
+{
+	App::ConsolePrintF("Current Cursor ID: 0x%x", CursorManager.GetActiveCursor());
+}
+
+const char* PrintCursorCheat::GetDescription(ArgScript::DescriptionMode mode) const
+{
+	if (mode == ArgScript::DescriptionMode::Basic) {
+		return "Prints the ID of the current cursor to the console.";
+	}
+	else {
+		return "PrintCursor: Prints the ID of the current cursor to the console.";
+	}
+}
